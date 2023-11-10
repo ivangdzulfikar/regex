e14059076bc7be212a945fe7b554413d751cdd50
ivangdzulfikar <122500001+ivangdzulfikar@users.noreply.github.com>
2023-11-10 20:47:36 +0700

    ignore case white matching

---
+++ ignore-case-while-matching.js
@@ -0,0 +1,7 @@
+let myString = "freeCodeCamp";
+let fccRegex = /freecodecamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+console.log(result);
+
+//use 'i' at the end of regex to ignore validation (?)
